b6ee6e0fe0b74649e5d6a58df83da5ed4a3ed161
Tsubasa Ogawa <hurikake@gmail.com>
2017-06-21 00:02:32 +0900

    get current time to set $date

--- weatherkun.pl
+++ weatherkun.pl
@@ -6,13 +6,24 @@ use LWP::UserAgent;
 use JSON;
 use Encode;
 use Data::Dumper;
+use Date::Simple;
 
 require './wkcommon.pl';
 require './get_weather.pl';
 
 my @coordinates = (139, 35);
-my $date = '201706182200';
+my $now = Date::Simple->new();
+my $date = $now->format('%Y%m%d%H%M');
 my $yahoo_app_id = WKCommon::get_yahoo_app_id();
+
+sub forecast_raining {
+  my @rainfalls = @_;
+  my $raining_flag = undef;
+
+  # processing
+
+  return $raining_flag;
+}
 
 my $request = GetWeather::get_request_instance(@coordinates, $yahoo_app_id, $date);
 my $response = GetWeather::get_response($request);
