0f8c04a010eb664bbb7b722d7e24dc6e32c0c100
Tsubasa Ogawa <hurikake@gmail.com>
2017-08-20 23:13:03 +0900

    use common script

--- weatherkun.pl
+++ weatherkun.pl
@@ -12,14 +12,11 @@ use Date::Simple;
 require './wkcommon.pl';
 require './get_weather.pl';
 
-my @coordinates = (35.681167, 139.767052);
-my $now = Date::Simple->new();
-my $date = $now->format('%Y%m%d%H%M');
-my $app_id = WKCommon::app_id();
-my $raining_threshold = 0.1;
+my ($sec, $min, $hour, $mday, $mon, $year, $wday, $yday, $isdst) = localtime(time);  
+my $date = sprintf("%04d/%02d/%02d %02d:%02d", $year + 1900, $mon + 1, $mday, $hour, $min);  
 
 sub get_rain_probability {
-  my $request = GetWeather::create_request_instance(@coordinates, $app_id);
+  my $request = GetWeather::create_request_instance(WKCommon::coordinates(), WKCommon::app_id());
   my $response = GetWeather::get_response($request);
 
   if(! $response->is_success) {
@@ -29,6 +26,7 @@ sub get_rain_probability {
   my $json = JSON->new->decode($response->content);
   my $rain_prob = $json->{hourly}->{data}[0]->{precipProbability};
 
+  printf STDERR "rain_prob: $rain_prob\n";
   return $rain_prob;
 }
 
@@ -36,18 +34,19 @@ sub will_it_rain {
   my $rain_prob = &get_rain_probability();
   my $raining_flag = 0;
 
-  $raining_flag = 1 if($rain_prob > $raining_threshold);
+  $raining_flag = 1 if($rain_prob > WKCommon::raining_threshold());
 
   return $raining_flag;
 }
 
 sub report_forecast {
-  print "Today it will rain.\n";
+  WKCommon::trigger_function();
   return 0;
 }
 
 # main
 
+system("echo '$date started' >> /var/log/weatherkun.log");
 if(&will_it_rain()) {
   my $report_ret = report_forecast();
   die 'report_forecast(): failed.' if($report_ret != 0);
